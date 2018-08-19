707dd16ad510711e3b34a9a536286ff8dd509485
sc0Vu <alk03073135@gmail.com>
2018-08-19 15:22:05 -0700

    Fix navbar-brand link

--- src/components/SagaHeader.vue
+++ src/components/SagaHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">SAGA</a>
+      <router-link tag="a" to="/" class="navbar-brand">SAGA</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
