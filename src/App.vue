56427767687e1ee753e3de7b25a30507427c621e
sc0Vu <alk03073135@gmail.com>
2018-08-19 13:26:27 -0700

    Add saga-footer element

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <saga-header></saga-header>
     <router-view/>
+    <saga-footer></saga-footer>
   </div>
 </template>
 
 <script>
 import SagaHeader from '@/components/SagaHeader'
+import SagaFooter from '@/components/SagaFooter'
+
 export default {
   name: 'app',
   components: {
-    SagaHeader
+    SagaHeader, SagaFooter
   }
 }
 </script>
---
+++ src/components/SagaFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+    <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'saga-footer'
+}
+</script>
+
+<style scoped>
+</style>
