6541cd878fd637e9672d0f0a759bda7a7ae8b8fc
Ben Lu <coopermilk123@gmail.com>
2018-08-24 20:13:05 -0700

    Support rooms API

--- src/store/modules/rooms.js
+++ src/store/modules/rooms.js
@@ -1,23 +1,44 @@
+import Api from '@/components/api'
+import moment from 'moment'
+
 const state = {
-  rooms: [
-    { id: 1, text: '...', today: true },
-    { id: 2, text: '...', today: false }
-  ]
+  rooms: []
 }
 
 // getters
 const getters = {
-  roomsToday: state => {
-    return state.rooms.filter(room => room.today)
+  getRooms (state) {
+    return state.rooms.map((room, index) => {
+      room.start_time = moment.unix(room.start_time).format('MMMM Do')
+      room.end_time = moment.unix(room.end_time).format('MMMM Do')
+      let imgs = [
+        'https://i.imgur.com/4LnD7e7.jpg',
+        'https://i.imgur.com/055DMYC.jpg',
+        'https://i.imgur.com/ougSPUF.jpg',
+        'https://i.imgur.com/fx5iK83.jpg',
+        'https://i.imgur.com/5RdQskc.jpg'
+      ]
+      room.img = imgs[index]
+      return room
+    })
   }
 }
 
 // actions
 const actions = {
+  async getRooms ({ commit }) {
+    let url = '/inventories'
+    let rooms = await Api.get(url)
+    rooms = rooms.data.inventories
+    commit('refreshHomePageRooms', rooms)
+  }
 }
 
 // mutations
 const mutations = {
+  refreshHomePageRooms (state, rooms) {
+    state.rooms = rooms
+  }
 }
 
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,54 @@
 <template>
-  <section class="home">
-    Home
-    {{ roomsToday }}
+  <section class="home container">
+    <div class="row">
+      <div class="col-lg-4" v-for="(room, index) in rooms" v-bind:key="index+room">
+        <div class="card mx-0 my-3">
+          <img class="card-img-top" :src="room.img" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">{{ room.title }} {{ room.inventory_id }}</h5>
+            <p class="card-text">{{ room.description }}</p>
+            <p class="card-text">
+              <span class="red-text">{{ room.start_time }}</span> ~ <span class="red-text">{{ room.end_time }}</span>
+            </p>
+            <a href="#" class="btn btn-primary">Book now</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'Home',
   computed: {
-    ...mapGetters([
-      'roomsToday'
-    ])
+    ...mapGetters({
+      rooms: 'getRooms'
+    })
+  },
+  async mounted () {
+    this.getRooms()
+  },
+  methods: {
+    ...mapActions(['getRooms'])
   }
 }
 </script>
 
 <style scoped>
+.card {
+  padding: 0px;
+  margin: 0px 15px;
+}
+.card-title {
+  color: rgba(0,0,0,.75);
+}
+.card-img-top {
+  height: 200px;
+}
+.red-text {
+  color: #0283df;
+}
 </style>
