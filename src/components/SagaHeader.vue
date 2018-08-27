65eda2cb7c09c1ff2e862e5702d195353a40a34c
sc0Vu <alk03073135@gmail.com>
2018-08-27 14:49:47 -0700

    Change header

--- src/components/SagaHeader.vue
+++ src/components/SagaHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-light">
-      <router-link tag="a" to="/" class="navbar-brand">SAGA</router-link>
+  <nav class="navbar navbar-expand-lg navbar-white">
+      <router-link tag="a" to="/" class="navbar-brand font-weight-bold">SAGA</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -16,13 +16,13 @@
             <a class="nav-link" href="#" v-show="isAuthenticated">Profile</a>
           </li>
           <li class="nav-item" v-show="isAuthenticated">
-            <a class="nav-link" v-on:click.prevent.self="logout">Logout</a>
+            <a class="nav-link" href="#" v-on:click.prevent.self="logout">Logout</a>
           </li>
           <router-link tag="li" to="/login" class="nav-item" v-show="!isAuthenticated">
             <a class="nav-link">Login</a>
           </router-link>
           <router-link tag="li" to="/signup" class="nav-item" v-show="!isAuthenticated">
-            <a class="nav-link">Sign up</a>
+            <a class="btn btn-warning text-white">Sign up</a>
           </router-link>
         </ul>
       </div>
@@ -61,6 +61,20 @@ export default {
 
 <style scoped>
 .navbar {
-  background-color: #515b62 !important;
+  background-color: #515b62;
+}
+
+.navbar-white {
+  padding-bottom: 0px;
+  background-color: #fff;
+  border-bottom: 1px solid #515b62;
+  box-shadow: 2px 2px 2px 1px rgba(81, 91, 98, 0.2);
+}
+.navbar-white > .navbar-brand, .navbar-white .navbar-nav > .nav-item > .nav-link {
+  color: #000;
+}
+
+.navbar-white .navbar-nav > .nav-item.active {
+  border-bottom: 2px solid #007bff;
 }
 </style>
